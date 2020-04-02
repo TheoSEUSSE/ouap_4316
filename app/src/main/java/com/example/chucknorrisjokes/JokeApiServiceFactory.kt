563bdc40b3fbdbf35ccf7c4e9d418815d5e3011c
Theo Seusse <th-eos@hotmail.fr>
2020-04-02 20:48:21 +0200

    Get random jokes

---
+++ app/src/main/java/com/example/chucknorrisjokes/JokeApiServiceFactory.kt
@@ -0,0 +1,21 @@
+package com.example.chucknorrisjokes
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.create
+
+object JokeApiServiceFactory {
+
+    fun factory() : JokeApiService {
+        val builder = Retrofit.Builder()
+            .baseUrl("https://api.chucknorris.io/jokes/random")
+            .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        return builder.create<JokeApiService>()
+
+    }
+}
--- app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.chucknorrisjokes
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView : RecyclerView
     private lateinit var viewManager : RecyclerView.LayoutManager
 
+    private val composite = CompositeDisposable()
+
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +33,17 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = JokeAdapter()
         }
+        val jokeService : Single<Joke> = JokeApiServiceFactory.factory().giveMeAJoke()
 
+        jokeService.subscribeOn(Schedulers.io())
+
+        val sub = jokeService.subscribeBy(
+                    onError = {Log.d("ERROR", it.toString())},
+                    onSuccess = {Log.d("SUCCESS", it.toString())}
+            )
+
+        composite.add(sub)
+        composite.clear()
 
     }
 }
