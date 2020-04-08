d8ff7243bb100df65a14b8e46d85616935238609
Theo Seusse <th-eos@hotmail.fr>
2020-04-08 17:43:01 +0200

    Multiple Jokes

--- app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/MainActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Scheduler
@@ -15,6 +17,7 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.serialization.UnstableDefault
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +31,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val viewAdapter = JokeAdapter()
+
         viewManager = LinearLayoutManager(this)
 
         recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
@@ -36,20 +41,28 @@ class MainActivity : AppCompatActivity() {
             adapter = viewAdapter
         }
 
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         val button = findViewById<Button>(R.id.joke_button)
-            button?.setOnClickListener() {
-                val jokeService : Single<Joke> = JokeApiServiceFactory.factory().giveMeAJoke()
 
-                val sub = jokeService.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribeBy(
+        button?.setOnClickListener() {
+            val jokeService : Single<Joke> = JokeApiServiceFactory.factory().giveMeAJoke()
+
+            jokeService.doOnSubscribe{ progressBar.visibility = View.VISIBLE }
+
+            val sub = jokeService
+                .repeat(10)
+                .delay(5,TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { progressBar.visibility = View.VISIBLE }
+                .doAfterNext { progressBar.visibility = View.INVISIBLE }
+                .subscribeBy(
                     onError = {Log.d("ERROR", it.toString())},
-                    onSuccess = {viewAdapter.listJoke = viewAdapter.listJoke.plus(it)}
-                )
-
-                composite.add(sub)
-            }
-
-
-        //composite.clear()
+                    onNext = {viewAdapter.listJoke = viewAdapter.listJoke.plus(it)},
+                    onComplete = {Log.d("COMPLETED", it.toString())}
+            )
+            composite.add(sub)
+        }
         Log.d("TEST", viewAdapter.listJoke.toString())
     }
 }
